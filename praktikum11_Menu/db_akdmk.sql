2801e74b10baf9f37f5d1dc92f8afc501ef0d787
Bagus Perdana Yusuf <62753559+bagusperdanay7@users.noreply.github.com>
2020-07-25 21:47:01 +0700

    Add files via upload

---
+++ praktikum11_Menu/db_akdmk.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community v9.63 
+MySQL - 5.5.5-10.4.11-MariaDB : Database - java_akdmk
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`java_akdmk` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `java_akdmk`;
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`nim`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `mahasiswa` */
+
+insert  into `mahasiswa`(`nim`,`nama`,`alamat`) values ('10107001','Justin Xavier','Bandung'),('10107002','Sanusi Rion','Jakarta');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
